248547e729ec92a4ca176d95ec5da94884b82363
inniy2 <inniy2@gmail.com>
2019-04-18 10:48:25 +0300

    shopper completed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,93 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Nav from './Nav';
 import './App.css';
+import ItemPage from './ItemPage';
+import {items} from './static-data';
+import CartPage from './CartPage';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+class App extends React.Component{
+
+    state = {
+        activeTab: 0,
+        cart: []
+    };
+
+    handleTabChange = (index) => {
+        this.setState({
+            activeTab: index
+        })
+    }
+
+    handleAddToCart = item => {
+        this.setState({
+            cart: [
+                ...this.state.cart,
+                item.id
+            ]
+        })
+
+        // console.log(this.state.cart.length);
+    }
+
+    handleRemoveOne = item => {
+        let index = this.state.cart.indexOf(item.id);
+        this.setState({
+            cart: [
+                ...this.state.cart.slice(
+                    0,
+                    index
+                ),
+                ...this.state.cart.slice(
+                    index + 1
+                )
+            ]
+        });
+    }
+
+    renderCart(){
+        console.log("What is cart: ", JSON.stringify(this.state.cart));
+        let itemCounts = this.state.cart.reduce((itemCounts, itemId) => {
+                console.log("itemCounts :", JSON.stringify(itemCounts), " item Id : ", itemId)
+                itemCounts[itemId] = itemCounts[itemId] || 0;
+                itemCounts[itemId]++;
+                console.log("return values :", JSON.stringify(itemCounts))
+                return itemCounts;
+            },{}
+            );
+        
+        let cartItems = Object.keys(itemCounts).map(itemId => {
+                var item = items.find( item => item.id === parseInt(itemId,10) )
+                return { ...item, count: itemCounts[itemId]};
+            });
+        return ( <CartPage items={cartItems} onAddOne={this.handleAddToCart} onRemoveOne={this.handleRemoveOne}/>)
+    }
+
+
+    renderContent(){
+        switch(this.state.activeTab){
+            default:
+                case 0:
+                    return( <ItemPage items={items} onAddToCart={this.handleAddToCart}/>)
+                case 1:
+                    return this.renderCart();
+        }
+        
+    };
+
+    render(){
+        let { activeTab } = this.state;
+
+        return(
+            <div className="App">
+                <Nav activeTab={activeTab} onTabChange={this.handleTabChange}/>
+                <main className="App-content">
+                    {this.renderContent()}
+                </main>
+            </div>
+        );
+    };
 }
 
-export default App;
+
+export default App
---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Nav = ({ activeTab, onTabChange }) =>
+    <nav className="App-nav">
+        <ul>
+            <li className={`App-nav-item ${activeTab === 0 && "selected"}`}>
+                <NavLink index={0} onClick={onTabChange}>Items</NavLink>
+            </li>
+            <li className={`App-nav-item ${activeTab === 1 && "selected"}`}>
+                <NavLink index={1} onClick={onTabChange}>Carts</NavLink>
+            </li>
+        </ul>
+    </nav>;
+
+
+class NavLink extends React.Component {
+    handleClick = () => {
+        this.props.onClick(
+            this.props.index
+        );
+    };
+
+    render(){
+        
+        return(
+            <a onClick={this.handleClick}>
+               {this.props.children}
+            </a>
+        );
+    }
+
+}
+
+export default Nav 
